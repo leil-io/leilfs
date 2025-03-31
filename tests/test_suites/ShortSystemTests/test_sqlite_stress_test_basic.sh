b4d7948d1a5203bcee753b7de35b91675611c279
Dave <dave@leil.io>
2025-03-31 23:48:19 +0200

    test: Test SQLite fix
    A couple of new tests were added to the ShortSystemTests and
    LongSystemTests suites. These tests intend to check every time that the
    SQLite workload is supported by our system.
    
    Signed-off-by: Dave <dave@leil.io>
    
    Co-authored-by: aNeutrino <pm@leil.io>

---
+++ tests/test_suites/ShortSystemTests/test_sqlite_stress_test_basic.sh
@@ -0,0 +1,32 @@
+timeout_set 5 minutes
+
+sqlite_test_path=$(realpath test_utils/sqlite_stress_test.py)
+
+CHUNKSERVERS=1 \
+	USE_RAMDISK=YES \
+	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER" \
+	setup_local_empty_saunafs info
+
+cd ${info[mount0]}
+
+cp $sqlite_test_path .
+sqlite_test=./sqlite_stress_test.py
+chmod +x $sqlite_test
+
+touch db
+
+run_sqlite_test_with_parameters() {
+	local nr_of_threads=$1
+	local nr_of_operations=$2
+	python3 $sqlite_test --threads=$nr_of_threads --operations_per_thread=$nr_of_operations \
+		--seed=1 --db_file=$(realpath db) |& tee $TEMP_DIR/sqlite_test.log
+	assert_equals 1 $(grep "Test completed without fatal errors." $TEMP_DIR/sqlite_test.log | wc -l)
+}
+
+run_sqlite_test_with_parameters 10 10
+run_sqlite_test_with_parameters 10 100
+run_sqlite_test_with_parameters 100 10
+run_sqlite_test_with_parameters 100 100
+run_sqlite_test_with_parameters 100 200
+run_sqlite_test_with_parameters 200 100
+run_sqlite_test_with_parameters 200 200
