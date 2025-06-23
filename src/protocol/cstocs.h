f76a5f5ba3d548efe5306ba08503bbc1df771930
Rolando Sánchez Ramos <rolysr@leil.io>
2025-06-24 01:24:42 +0200

    chore!: Deprecate CS to CS protocol
    This commit removes all code on the cstocs.h file from protocol
    codes related with the LizardFS versions equal or older than
    v3.12.0. This change will be part of the SaunaFS v5.0.0 release,
    where support to older LizardFS versions is not longer required.
    
    BREAKING CHANGE: Communication among chunkservers might not
    work for binaries from LizardFS v3.12.0 or older.
    
    Signed-off-by: Rolando Sánchez Ramos <rolysr@leil.io>

--- src/protocol/cstocs.h
+++ src/protocol/cstocs.h
@@ -29,19 +29,8 @@ namespace cstocs {
 
 namespace getChunkBlocks {
 
-const PacketVersion kStandardAndXorChunks = 0;
 const PacketVersion kECChunks = 1;
 
-inline void serialize(std::vector<uint8_t>& destination,
-		uint64_t chunkId, uint32_t chunkVersion, const legacy::ChunkPartType& chunkType) {
-	serializePacket(destination, SAU_CSTOCS_GET_CHUNK_BLOCKS, kStandardAndXorChunks, chunkId, chunkVersion, chunkType);
-}
-
-inline void deserialize(const uint8_t* source, uint32_t sourceSize,
-		uint64_t& chunkId, uint32_t& chunkVersion, legacy::ChunkPartType& chunkType) {
-	verifyPacketVersionNoHeader(source, sourceSize, kStandardAndXorChunks);
-	deserializeAllPacketDataNoHeader(source, sourceSize, chunkId, chunkVersion, chunkType);
-}
 
 inline void serialize(std::vector<uint8_t>& destination,
 		uint64_t chunkId, uint32_t chunkVersion, const ChunkPartType& chunkType) {
@@ -58,22 +47,7 @@ inline void deserialize(const uint8_t* source, uint32_t sourceSize,
 
 namespace getChunkBlocksStatus {
 
-const PacketVersion kStandardAndXorChunks = 0;
 const PacketVersion kECChunks = 1;
-
-inline void serialize(std::vector<uint8_t>& destination,
-		uint64_t chunkId, uint32_t chunkVersion, const legacy::ChunkPartType& chunkType,
-		uint16_t blocks, uint8_t status) {
-	serializePacket(destination, SAU_CSTOCS_GET_CHUNK_BLOCKS_STATUS, kStandardAndXorChunks, chunkId, chunkVersion,
-			chunkType, blocks, status);
-}
-
-inline void deserialize(const std::vector<uint8_t>& source,
-		uint64_t& chunkId, uint32_t& chunkVersion, legacy::ChunkPartType& chunkType,
-		uint16_t& blocks, uint8_t& status) {
-	verifyPacketVersionNoHeader(source, kStandardAndXorChunks);
-	deserializeAllPacketDataNoHeader(source, chunkId, chunkVersion, chunkType, blocks, status);
-}
 
 inline void serialize(std::vector<uint8_t>& destination,
 		uint64_t chunkId, uint32_t chunkVersion, const ChunkPartType& chunkType,
