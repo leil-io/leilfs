834e962037e7904a085ef7268fddf16fb4188560
guillex <guillex@leil.io>
2025-07-04 17:15:57 +0200

    fix(wireshark): Fix packet comments for wireshark
    The comment of the following packets were fixed to be understood by the
    script that generates the wireshark plugin:
    
    SAU_CLTOMA_WHOLE_PATH_LOOKUP
    - Now uses STRING32, which means the size before the string content
      will be encoded in 32 bits.
    
    SAU_MATOCL_WHOLE_PATH_LOOKUP
    - The version 1 was not provided as comment, the wireshark generation
      script relies on the comment.
    
    Co-authored-by: aNeutrino <piotr.modrzyk@gmail.com>
    
    Signed-off-by: guillex <guillex@leil.io>

--- tests/test_suites/SanityChecks/test_wireshark_plugin_generation.sh
+++ tests/test_suites/SanityChecks/test_wireshark_plugin_generation.sh
@@ -1,10 +1,24 @@
-cd "$TEMP_DIR"
-cp -av "${SOURCE_DIR}/utils/wireshark/plugins/epan/saunafs" .
-rm -f saunafs/*.c
+assert_program_installed python3
 
-if is_program_installed python3 ; then
-	assert_success saunafs/generate.sh "$SOURCE_DIR/src/protocol/SFSCommunication.h"
-	assert_success test -s saunafs/packet-saunafs.c
-else
-	echo "python3 is not installed on your system hence wireshark plugin won't be build."
-fi
+cd "${TEMP_DIR}"
+cp -av "${SOURCE_DIR}/utils/wireshark/plugins/epan/saunafs" .
+output_file=saunafs/packet-saunafs.c
+SFSCommunication_h="${SOURCE_DIR}/src/protocol/SFSCommunication.h"
+
+test_wireshark_version() {
+    local version=$1
+    local expected_string=$2
+    local unexpected_string=$3
+
+    rm -f ${output_file}
+    assert_success saunafs/generate.sh "${SFSCommunication_h}" "${version}"
+    assert_success test -s ${output_file}
+    assert_success grep --quiet "${expected_string}" ${output_file}
+    assert_failure grep --quiet "${unexpected_string}" ${output_file}
+}
+
+# Run all test cases
+test_wireshark_version "4.0.0" "tvb_get_guint8" "tvb_get_uint8"
+test_wireshark_version "4.5.0" "tvb_get_uint8" "tvb_get_guint8"
+
+rm -f ${output_file}
