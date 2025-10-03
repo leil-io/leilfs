521f855f8fc024480b1e17dbf76ed8ba51da0b24
Rolando Sánchez Ramos <rolysr@leil.io>
2025-10-03 22:30:52 +0200

    test: Add soft and hard links for path by inode
    This commit adds corrresponding checks on the test for path by inode
    feature, allowing it to cover more cases of its usage.
    
    Signed-off-by: Rolando Sánchez Ramos <rolysr@leil.io>

--- tests/test_suites/ShortSystemTests/test_path_by_inode.sh
+++ tests/test_suites/ShortSystemTests/test_path_by_inode.sh
@@ -16,3 +16,24 @@ chmod 200 folder
 assert_failure cat .saunafs_path_by_inode/3
 assert_failure cat .saunafs_path_by_inode/5
 assert_failure cat .saunafs_path_by_inode/6
+
+# test path by inode for softlinks and hardlinks
+touch file5
+echo "data_file5" > file5
+
+ln -s file5 softlink_to_file
+ln file5 hardlink_to_file
+
+assert_equals "file5" "$(cat .saunafs_path_by_inode/8)"
+assert_equals "data_file5" "$(cat $(cat .saunafs_path_by_inode/8))"
+
+# check that inode of softlink is a new one
+file5_inode=$(stat -c %i file5)
+softlink_inode=$(stat -c %i softlink_to_file)
+assert_not_equal "$softlink_inode" "$file5_inode"
+assert_equals "softlink_to_file" "$(cat .saunafs_path_by_inode/9)"
+assert_equals "data_file5" "$(cat $(cat .saunafs_path_by_inode/9))"
+
+# check that inode of hardlink is the same as the original file
+file5_hardlink_inode=$(stat -c %i hardlink_to_file)
+assert_equals "$file5_inode" "$file5_hardlink_inode"
