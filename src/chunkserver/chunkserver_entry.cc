2416d70be7fac135b862f39f51ec92e49bd08715
David Manuel García Aguilera <dave@leil.io>
2025-10-15 01:14:36 +0200

    fix(cs): Fix replica writes - DISCONNECTED error (#626)
    It was noticed that in some scenarios the replica writes might fail,
    and the error code sent from the chunkservers is DISCONNECTED. This
    is caused due to a misshandled case when writing to a socket from the
    InputBuffer code. This function was returning 0: causing an immediate
    stop in the forward operation and sending through the chain the
    previously mentioned error in some cases when it was supposed to return
    -1 and use the errno (EAGAIN) to retry the write. The fix consists in
    analizing whether the block write had some previous header data written
    to the socket and return the expected error when a block write fails
    with EAGAIN errno.
    
    A similar fix was applied preemptively to the reads to the InputBuffer.
    Also the missing places where the fwdError function was called were
    logged for better future debugging.
    
    Signed-off-by: Dave <dave@leil.io>

--- src/chunkserver/chunkserver_entry.cc
+++ src/chunkserver/chunkserver_entry.cc
@@ -223,10 +223,12 @@ void ChunkserverEntry::retryConnect() {
 
 	if (connectRetryCounter < kConnectRetries) {
 		if (initConnection() < kInitConnectionOK) {
+			safs::log_info("({}) Failed initializing connection.", __func__);
 			fwdError();
 			return;
 		}
 	} else {
+		safs::log_info("({}) Connect retry counter reached limit.", __func__);
 		fwdError();
 		return;
 	}
@@ -1179,6 +1181,7 @@ bool ChunkserverEntry::processRWBytes(int bytesRW, PacketStruct &packet, bool sh
                                       const char *callerName, bool isRead) {
 	if (bytesRW == 0) {
 		if (shouldForwardError) {
+			safs::log_info("({}) {} returned 0 bytes", callerName, isRead ? "read" : "write");
 			fwdError();
 		} else {
 			state = State::Close;
