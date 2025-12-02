f2a3654670c17a97cc2e95152a73ee74d73a0648
GigaCronos <jorge.cabrera@leil.io>
2025-12-02 04:28:07 -0500

    Initial Debug

---
+++ tests/test_suites/LongSystemTests/test_build_saunafs.sh
@@ -0,0 +1,36 @@
+timeout_set 10 minutes
+assert_program_installed git
+assert_program_installed cmake
+
+CHUNKSERVERS=8 \
+	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER|cacheexpirationtime=0" \
+	CHUNKSERVER_EXTRA_CONFIG="READ_AHEAD_KB = 1024|MAX_READ_BEHIND_KB = 2048"
+	setup_local_empty_saunafs info
+
+MINIMUM_PARALLEL_JOBS=4
+MAXIMUM_PARALLEL_JOBS=16
+PARALLEL_JOBS=$(get_nproc_clamped_between ${MINIMUM_PARALLEL_JOBS} ${MAXIMUM_PARALLEL_JOBS})
+
+echo "DEBUG 1"
+
+cd ${info[mount0]}
+
+mkdir work
+saunafs setgoal ec43 work
+
+echo "DEBUG 2"
+
+cd work
+
+assert_success git clone "https://github.com/leil-io/saunafs.git"
+
+echo "DEBUG 3"
+
+cd saunafs
+
+mkdir -p build
+echo "DEBUG 4"
+cd build
+echo "DEBUG 5"
+assert_success cmake .. -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=RelWithDebInfo
+assert_success make -j${PARALLEL_JOBS}
