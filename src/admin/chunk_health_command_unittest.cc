leil-io/saunafs

feat(master): date the chunk health report
A metadata server that keeps its chunk counters current as chunks
change can answer the health request from them at any time. A
backend that measures those counters in the background instead has
only its last measurement, and a count read without its date cannot
be told from one nobody has taken yet: every counter is zero before
the first measurement completes.

The chunksHealth packets gain version 1. The request drops the
regular-only flag no server ever honoured; the answer adds whether
the counters come from a measurement, the measurement's generation,
start and end, chunks scanned and excluded, and the server's clock,
so a client can age it without trusting its own. Version 0 keeps
the released bytes both ways. The chunk operations interface gains
getHealthFreshness, empty for the in-memory backend, whose counters
have no measurement to date.

For the in-memory backend nothing observable changes except that a
version 1 request is answered, with the same counters and no
measurement, where the released server closed the connection. The
version exists for backends that will fill the measurement in; this
change only makes room for it.

saunafs-admin chunks-health asks in version 1. When the server
closes the connection before sending any byte of an answer, as an
older server does with an unknown version, it says so on stderr and
asks again with the original request on a fresh connection; any
other failure is reported as it is. It prints the measurement before
the counts, or a MEA porcelain row after them, only when the server
measures. Unit tests cover both packet versions and the report text;
the shared tests compare counts without that row and wait for a
measurement that covers the cluster as they left it, on a backend
that reports one.

Signed-off-by: guillex <guillex@leil.io>

src/admin/chunk_health_command_unittest.cc
@@ -1,0 +1,68 @@
+/*
+   Copyright 2026 Leil Storage
+
+   This file is part of SaunaFS.
+
+   SaunaFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   SaunaFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with SaunaFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/platform.h"
+
+#include <gtest/gtest.h>
+
+#include "admin/chunk_health_command.h"
+
+namespace {
+
+ChunkHealthFreshness measuredFreshness() {
+	ChunkHealthFreshness freshness;
+	freshness.generation = 41;
+	freshness.scanStart = 1789126800;
+	freshness.scanEnd = 1789126803;
+	freshness.chunksScanned = 123456;
+	freshness.chunksExcluded = 7;
+	freshness.chunkserversDown = 2;
+	return freshness;
+}
+
+}  // namespace
+
+TEST(ChunksHealthCommandTests, RowBeforeAnyMeasurement) {
+	EXPECT_EQ("MEA 0 0 0 0 0 0", ChunksHealthCommand::freshnessRow(ChunkHealthFreshness(), 1000));
+}
+
+TEST(ChunksHealthCommandTests, RowCarriesTheSixFields) {
+	// Age is the server clock past the scan end, duration the scan end past its start.
+	EXPECT_EQ("MEA 41 7 3 123456 7 2",
+	          ChunksHealthCommand::freshnessRow(measuredFreshness(), 1789126810));
+}
+
+TEST(ChunksHealthCommandTests, RowNeverGoesNegative) {
+	auto freshness = measuredFreshness();
+	freshness.scanEnd = freshness.scanStart - 1;
+	EXPECT_EQ("MEA 41 0 0 123456 7 2",
+	          ChunksHealthCommand::freshnessRow(freshness, freshness.scanEnd - 5));
+}
+
+TEST(ChunksHealthCommandTests, SummaryBeforeAnyMeasurement) {
+	EXPECT_EQ(
+	    "Chunk health has not been measured yet, so the counts below are not a statement about"
+	    " the installation.",
+	    ChunksHealthCommand::freshnessSummary(ChunkHealthFreshness(), 1000));
+}
+
+TEST(ChunksHealthCommandTests, SummaryDatesTheMeasurement) {
+	EXPECT_EQ(
+	    "Measured 7s ago (scan 41 took 3s, 123456 chunks, 7 excluded, 2 chunkservers unreachable)",
+	    ChunksHealthCommand::freshnessSummary(measuredFreshness(), 1789126810));
+}

src/common/chunk_health_freshness.h
@@ -1,0 +1,52 @@
+/*
+   Copyright 2026      Leil Storage OÜ
+
+   This file is part of SaunaFS.
+
+   SaunaFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   SaunaFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with SaunaFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/platform.h"
+
+#include <cstdint>
+
+#include "common/serialization_macros.h"
+
+/// When and over what a chunk health answer was measured. A backend that scans its records
+/// answers complete but older than the request, and a monitor must tell "nothing lost, measured
+/// a minute ago" from "nothing measured yet". A backend whose counters are current sends none.
+struct ChunkHealthFreshness {
+	/// Scan that produced the counters. Zero means no scan has completed yet, so the counters
+	/// are all zero because nothing has been measured, not because nothing is wrong.
+	uint64_t generation = 0;
+
+	/// Unix timestamps bounding the scan. The distance between them is how long the scan took.
+	uint32_t scanStart = 0;
+	uint32_t scanEnd = 0;
+
+	/// Chunk records the scan read, and how many of those it could not classify and left out
+	/// of the counters (a record being written while the scan passed it, for instance).
+	uint64_t chunksScanned = 0;
+	uint64_t chunksExcluded = 0;
+
+	/// Chunkservers the scan counted as unreachable; their copies count as absent, so this is
+	/// what separates a report that reads as data loss from one that reads as servers down.
+	uint32_t chunkserversDown = 0;
+
+	bool measured() const { return generation != 0; }
+
+	SAUNAFS_DEFINE_SERIALIZE_METHODS(generation, scanStart, scanEnd, chunksScanned, chunksExcluded,
+	                                 chunkserversDown);
+};

leil-io/leilfs

src/admin/chunk_health_command_unittest.cc
@@ -1,0 +1,68 @@
+/*
+   Copyright 2026      Leil Storage OÜ
+
+   This file is part of LeilFS.
+
+   LeilFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   LeilFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LeilFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/platform.h"
+
+#include <gtest/gtest.h>
+
+#include "admin/chunk_health_command.h"
+
+namespace {
+
+ChunkHealthFreshness measuredFreshness() {
+	ChunkHealthFreshness freshness;
+	freshness.generation = 41;
+	freshness.scanStart = 1789126800;
+	freshness.scanEnd = 1789126803;
+	freshness.chunksScanned = 123456;
+	freshness.chunksExcluded = 7;
+	freshness.chunkserversDown = 2;
+	return freshness;
+}
+
+}  // namespace
+
+TEST(ChunksHealthCommandTests, RowBeforeAnyMeasurement) {
+	EXPECT_EQ("MEA 0 0 0 0 0 0", ChunksHealthCommand::freshnessRow(ChunkHealthFreshness(), 1000));
+}
+
+TEST(ChunksHealthCommandTests, RowCarriesTheSixFields) {
+	// Age is the server clock past the scan end, duration the scan end past its start.
+	EXPECT_EQ("MEA 41 7 3 123456 7 2",
+	          ChunksHealthCommand::freshnessRow(measuredFreshness(), 1789126810));
+}
+
+TEST(ChunksHealthCommandTests, RowNeverGoesNegative) {
+	auto freshness = measuredFreshness();
+	freshness.scanEnd = freshness.scanStart - 1;
+	EXPECT_EQ("MEA 41 0 0 123456 7 2",
+	          ChunksHealthCommand::freshnessRow(freshness, freshness.scanEnd - 5));
+}
+
+TEST(ChunksHealthCommandTests, SummaryBeforeAnyMeasurement) {
+	EXPECT_EQ(
+	    "Chunk health has not been measured yet, so the counts below are not a statement about"
+	    " the installation.",
+	    ChunksHealthCommand::freshnessSummary(ChunkHealthFreshness(), 1000));
+}
+
+TEST(ChunksHealthCommandTests, SummaryDatesTheMeasurement) {
+	EXPECT_EQ(
+	    "Measured 7s ago (scan 41 took 3s, 123456 chunks, 7 excluded, 2 chunkservers unreachable)",
+	    ChunksHealthCommand::freshnessSummary(measuredFreshness(), 1789126810));
+}

src/common/chunk_health_freshness.h
@@ -1,0 +1,52 @@
+/*
+   Copyright 2026      Leil Storage OÜ
+
+   This file is part of LeilFS.
+
+   LeilFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   LeilFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LeilFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/platform.h"
+
+#include <cstdint>
+
+#include "common/serialization_macros.h"
+
+/// When and over what a chunk health answer was measured. A backend that scans its records
+/// answers complete but older than the request, and a monitor must tell "nothing lost, measured
+/// a minute ago" from "nothing measured yet". A backend whose counters are current sends none.
+struct ChunkHealthFreshness {
+	/// Scan that produced the counters. Zero means no scan has completed yet, so the counters
+	/// are all zero because nothing has been measured, not because nothing is wrong.
+	uint64_t generation = 0;
+
+	/// Unix timestamps bounding the scan. The distance between them is how long the scan took.
+	uint32_t scanStart = 0;
+	uint32_t scanEnd = 0;
+
+	/// Chunk records the scan read, and how many of those it could not classify and left out
+	/// of the counters (a record being written while the scan passed it, for instance).
+	uint64_t chunksScanned = 0;
+	uint64_t chunksExcluded = 0;
+
+	/// Chunkservers the scan counted as unreachable; their copies count as absent, so this is
+	/// what separates a report that reads as data loss from one that reads as servers down.
+	uint32_t chunkserversDown = 0;
+
+	bool measured() const { return generation != 0; }
+
+	SAUNAFS_DEFINE_SERIALIZE_METHODS(generation, scanStart, scanEnd, chunksScanned, chunksExcluded,
+	                                 chunkserversDown);
+};